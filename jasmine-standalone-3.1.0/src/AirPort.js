5f393ce39a0b9787344a2e541fa153e2c46cf66e
Patryk Pilecki <ppilecki@icloud.com>
2018-04-17 16:22:31 +0100

    throws an error msg

--- jasmine-standalone-3.1.0/src/AirPort.js
+++ jasmine-standalone-3.1.0/src/AirPort.js
@@ -5,10 +5,16 @@ function AirPort() {
 
 };
 AirPort.prototype.land = function(plane) {
+  if (this.stormy() === true)
+    throw new Error(["It is Stormy!"])
+  else
   plane.land()
   this.hanger.push(plane);
 };
 AirPort.prototype.takeoff = function(plane) {
+  if (this.stormy() === true)
+    throw new Error(["It is Stormy!"])
+  else
   plane.takeoff()
   var planePosition = this.hanger.indexOf(plane)
   this.hanger.splice(planePosition, 1);
