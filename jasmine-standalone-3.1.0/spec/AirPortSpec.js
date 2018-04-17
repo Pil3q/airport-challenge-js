488a5cc26fc1212a69225d26a07faea9e79e9dff
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-04-17 18:05:22 +0100

    added default capacity, takeoff and landing errors, stormy errors

--- jasmine-standalone-3.1.0/spec/AirPortSpec.js
+++ jasmine-standalone-3.1.0/spec/AirPortSpec.js
@@ -1,22 +1,48 @@
 describe('AirPort', function(){
   var airport = new AirPort();
   var plane = new AirPlane();
+
   it('should land plane', function(){
+    spyOn(airport, "stormy").and.returnValue(false);
     airport.land(plane);
     expect(airport.hanger).toContain(plane)
   })
+
   it('should takeoff plane', function(){
+    spyOn(airport, "stormy").and.returnValue(false);
     airport.takeoff(plane);
     expect(airport.hanger).not.toContain(plane)
   })
-  it('has a weather', function(){
-    expect(airport.weather).toContain('fine' || 'stormy')
+
+  it ('stubs the weather to stormy and expects error when landing', function() {
+    var stormyAirport = new AirPort();
+    spyOn(stormyAirport, "stormy").and.returnValue(true);
+    expect( function(){ stormyAirport.land(plane); } ).toThrow(new Error("the weather is stormy, cant land"));
   })
-  it('hrespond to stormy with random  true/false', function(){
-    var result = []
-    for (var i = 0; i <= 20; i++) {
-    result.push(airport.stormy())
-    }
-    expect(result).toContain(true && false)
+
+  it ('stubs the weather to stormy and expects error when taking off', function() {
+    var stormyAirport = new AirPort();
+    spyOn(stormyAirport, "stormy").and.returnValue(true);
+    expect( function(){ stormyAirport.takeoff(plane); } ).toThrow(new Error("the weather is stormy, cant take off"));
   })
+
+  it ('prevents landing in a full airport', function() {
+    var fullAirport = new AirPort();
+    spyOn(fullAirport, "stormy").and.returnValue(false);
+    spyOn(fullAirport, "full").and.returnValue(true);
+    expect( function(){ fullAirport.land(plane); } ).toThrow(new Error("airport is full, cant land"));
+  })
+
+  it ('prevents taking off in a full airport', function() {
+    var fullAirport = new AirPort();
+    spyOn(fullAirport, "stormy").and.returnValue(false);
+    spyOn(fullAirport, "full").and.returnValue(true);
+    expect( function(){ fullAirport.takeoff(plane); } ).toThrow(new Error("airport is full, cant take off"));
+  })
+
+  it ('sets a new capacity', function() {
+    airport.setcapacity(10);
+    expect(airport.capacity).toEqual(10);
+  })
+
 })
--- jasmine-standalone-3.1.0/src/AirPort.js
+++ jasmine-standalone-3.1.0/src/AirPort.js
@@ -2,16 +2,28 @@ function AirPort() {
   this.hanger = []
   var airportWeather = new Weather
   this.weather = airportWeather.today()
+  this.capacity = 5
 
 };
 AirPort.prototype.land = function(plane) {
-  plane.land()
-  this.hanger.push(plane);
+  if (this.stormy() === true)
+    throw new Error("the weather is stormy, cant land")
+  else if (this.full() === true)
+    throw new Error("airport is full, cant land")
+  else
+    plane.land()
+    this.hanger.push(plane);
+
 };
 AirPort.prototype.takeoff = function(plane) {
-  plane.takeoff()
-  var planePosition = this.hanger.indexOf(plane)
-  this.hanger.splice(planePosition, 1);
+  if (this.stormy() === true)
+    throw new Error("the weather is stormy, cant take off")
+  else if (this.full() === true)
+    throw new Error("airport is full, cant take off")
+  else
+    plane.takeoff()
+    var planePosition = this.hanger.indexOf(plane)
+    this.hanger.splice(planePosition, 1);
 };
 AirPort.prototype.stormy = function() {
   if (this.weather === 'stormy')
@@ -19,3 +31,14 @@ AirPort.prototype.stormy = function() {
   else
     return false
 };
+
+AirPort.prototype.full = function() {
+  if (this.hanger.length >= this.capacity)
+    return true
+    else
+      return false
+};
+
+AirPort.prototype.setcapacity = function(capacity) {
+  this.capacity = capacity
+}
